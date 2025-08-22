590092d831b3ce9132b7070e03c01e268d4b80d4
rewan24 <rawanazab038@gmail.com>
2025-08-23 01:15:46 +0300

    new apps update

--- backend/groups/models.py
+++ backend/groups/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Group(models.Model):
-
     name = models.CharField(max_length=100, unique=True)
     capacity = models.PositiveIntegerField()
     schedule = models.CharField(max_length=100)
     students = models.ManyToManyField("students.Student", blank=True, related_name="groups")
+    days = models.CharField(max_length=100, blank=True, help_text="اكتب مواعيد المجموعة مثل: سبت، تلات")
 
     def __str__(self):
-        return f"{self.name} - {self.schedule}"
+        return f"{self.name} - {self.schedule} - {self.days}"
